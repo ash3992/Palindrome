2624e5a3a11746e7d2f6f460fd191c2a2c34ff25
ash3992 <50383432+ash3992@users.noreply.github.com>
2021-12-10 16:08:47 -0500

    push

--- Palindrome.playground/Contents.swift
+++ Palindrome.playground/Contents.swift
@@ -1,9 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
 
 
 
+func findSecondLargestValue(array: [Int]) -> Int{
+    
+    var newArray = array.sorted()
+    var array2 = [Int]()
+    var num = 0
+    
+    for i in newArray{
+            num += 1
+            array2.append(i)
+    }
+    
+    var num1 = array2[array2.count - 2]
+    return num1
+}
+
+print(findSecondLargestValue(array: [1,4,2,8,9]))
+print(findSecondLargestValue(array: [323,4100,3,20000,838,4]))
 
 func palindromeChecker(string: String) -> Bool{
     
